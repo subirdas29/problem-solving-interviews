771223f5b6355f21b4571a76ddb7c291bc0cb5c4
subirdas29 <subirdas1045@gmail.com>
2025-04-13 12:54:22 +0600

    new added

--- task/task5.ts
+++ task/task5.ts
@@ -1,3 +1,22 @@
-// 5.Task: Find and Modify
+//? 5.Task: Find and Modify
 
-// Write a function that searches an array of objects for a specific person by name. If found, modify their age property. Print the updated array.
+//? Write a function that searches an array of objects for a specific person by name. If found, modify their age property. Print the updated array.
+
+const persons = [
+    {
+        name:'Subir',
+        age:24,
+    },
+    {
+        name:'Joy',
+        age:26
+    },
+    {
+        name:'Durjoy',
+        age:20
+    }
+]
+
+const allPerson = (persons)=>{
+    const findPerson = 
+}
